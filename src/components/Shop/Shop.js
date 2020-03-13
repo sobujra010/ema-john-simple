28e6f7d4b174188b40833740d52a0a566a93ac69
sobujra010 <dreamer.sobuj.010@gmail.com>
2020-03-13 20:11:36 +0600

    shop page coding done

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import fakeData from '../../fakeData';
+import './Shop.css';
+import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
+
+const Shop = () => {
+    const first10 = fakeData.slice(0, 10);
+    const [products, setProduct] = useState(first10);
+    const [cart, setCart] = useState([]);
+
+    const handleAddProduct = (product) =>{
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
+
+
+    return (
+        <div className="shop-container">
+            <div className="product-container">           
+                {
+                    products.map(pd =><Product 
+                        handleAddProduct = {handleAddProduct}
+                        Product={pd}></Product>)
+                }           
+            </div>
+            <div className="cart-container">
+               <Cart cart={cart}></Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
